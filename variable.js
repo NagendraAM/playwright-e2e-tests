378defca9e907b029dbff005abad02092396c1e8
NagendraAM <60282008+NagendraAM@users.noreply.github.com>
2026-06-14 12:25:16 +0530

    first commit

---
+++ variable.js
@@ -0,0 +1,27 @@
+/**
+ * Adds 2 numbers
+ * @param {*} n1
+ * @param {*} n2
+ * @returns addition of 2 numbers
+ */ 
+function sum(n1, n2) { 
+    let greeting = "Hello and Welcome!!"; 
+    console.log(greeting); 
+    return n1 + n2; 
+} 
+
+// Print the output of the function
+console.log("The Output of this is:", sum(4, 5)); 
+
+/**
+ --Variable Rules--
+
+1. Variable name should start with a letter, or underscore (_), or dollar sign ($)
+2. System reserved keywords can’t be used e.g. if, else, throw, break
+3. Variable names are case-sensitive
+4. Can’t have same name for other types (e.g. function name, object name)
+5. Multiple variables can be declared in single line (separated by comma)
+6. When you just declare a variable, the data type will be undefined
+ 
+ */
+
